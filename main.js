b1c7d65860b7d37a8d32ea642ab830ec82370f73
Abiodun <abiodunhodonu@gmail.com>
2023-06-23 22:44:19 +0100

    top navbar scroll

--- main.js
+++ main.js
@@ -32,3 +32,9 @@ faqs.forEach(faq => {
         }
     })
 })
+
+//add background to nav
+
+window.addEventListener('scroll', () => {
+    document.querySelector ('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
